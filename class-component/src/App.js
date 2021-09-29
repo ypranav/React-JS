5db884964eaa461424b47202cbe27e2ae05abbd1
ypranav <pranavpanhale2@gmail.com>
2021-09-29 14:04:03 +0530

    Add files via upload

---
+++ class-component/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'; 
+import './App.css';
+import Welcome from './components/Welcome';
+
+                                  //class component 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <Welcome />
+      </div>  
+    );
+  }
+}
+
+export default App;
