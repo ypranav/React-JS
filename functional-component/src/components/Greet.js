53531a927993c57b45af80208d919638ae371e5d
ypranav <pranavpanhale2@gmail.com>
2021-09-29 12:42:39 +0530

    Add files via upload

---
+++ functional-component/src/components/Greet.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// function Greet() {
+//     return <h1> This is the Functional Component </h1>
+// }
+
+const Greet = () => <h2> Arrow Function</h2>
+export default Greet; 
